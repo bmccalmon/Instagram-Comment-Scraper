0fd94c50e93d48f42f5f0e9bccf085f52d52c479
benmccalmon <benmccalmon@gmail.com>
2024-04-18 14:23:37 -0400

    cleaned up code

--- parse_comments.py
+++ parse_comments.py
@@ -3,11 +3,13 @@ from bs4 import BeautifulSoup
 import sys
 import csv
 
+content_class_names = "_ap3a _aaco _aacu _aacx _aad7 _aade"
+likes_class_names = "x193iq5w xeuugli x1fj9vlw x13faqbe x1vvkbs xt0psk2 x1i0vuye x1fhwpqd x1s688f x1roi4f4 x10wh9bi x1wdrske x8viiok x18hxmgj"
+output_file = "comments_output.csv"
+
 def repair_likes(likes):
     fixed_likes = list()
-    for i in range(len(likes)):
-        if i >= len(likes) - 1:
-            break
+    for i in range(len(likes) - 1):
         if likes[i].text != "Reply" and likes[i].text != "See translation":
             fixed_likes.append(likes[i].text)
         else:
@@ -16,13 +18,13 @@ def repair_likes(likes):
     return fixed_likes
 
 def extract_comments(post_file_path):
-    with open(post_file_path, 'r', encoding='utf-8') as file:
+    with open(post_file_path, "r", encoding="utf-8") as file:
         html_content = file.read()
 
-    soup = BeautifulSoup(html_content, 'html.parser')
+    soup = BeautifulSoup(html_content, "html.parser")
 
-    contents = soup.find_all('span', class_="_ap3a _aaco _aacu _aacx _aad7 _aade")
-    likes = soup.find_all('span', class_="x193iq5w xeuugli x1fj9vlw x13faqbe x1vvkbs xt0psk2 x1i0vuye x1fhwpqd x1s688f x1roi4f4 x10wh9bi x1wdrske x8viiok x18hxmgj")
+    contents = soup.find_all("span", class_=content_class_names)
+    likes = soup.find_all("span", class_=likes_class_names)
 
     likes = repair_likes(likes)
 
@@ -35,11 +37,11 @@ def extract_comments(post_file_path):
 def export_to_csv(extracted_comments):
     header = ["Comment", "Likes"]
     data = extracted_comments
-    filename = 'comments_output.csv'
+    filename = output_file
     with open(filename, 'w', newline="") as file:
-        csvwriter = csv.writer(file) # 2. create a csvwriter object
-        csvwriter.writerow(header) # 4. write the header
-        csvwriter.writerows(data) # 5. write the rest of the data
+        csvwriter = csv.writer(file)
+        csvwriter.writerow(header)
+        csvwriter.writerows(data)
 
 def main():
     extracted_comments = extract_comments(sys.argv[1])
@@ -47,9 +49,7 @@ def main():
     export_to_csv(extracted_comments)
     for comment in extracted_comments:
         print("    • " + comment[0])
-        #print("          LIKES: " + comment[1])
-        print("            " + comment[1])
-        pass
+        print("          LIKES: " + comment[1])
 
 if __name__ == "__main__":
     main()
